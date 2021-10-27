54f8d471e2e22248e301b263e932711df6313643
Mukagali-Makhmudov <mukagali.mahmudov@gmail.com>
2021-10-27 23:51:54 +0600

    commit1

---
+++ lib/common/const/color_const.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color mainBackColor = Color.fromRGBO(50, 60, 80, 1);
+  static const Color cardColor = Color.fromRGBO(50, 60, 80, 0.7);
+  static const Color textWhite = Colors.white;
+}
---
+++ lib/domain/exercise.dart
@@ -0,0 +1,8 @@
+
+class Exercise {
+  String muscleGroup;
+  String name;
+  String type;
+
+  Exercise({required this.name, required this.muscleGroup, required this.type});
+}
