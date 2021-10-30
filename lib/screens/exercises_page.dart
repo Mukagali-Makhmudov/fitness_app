559f7de300d56778db8483a93eb40dc6ffbe4318
Mukagali-Makhmudov <mukagali.mahmudov@gmail.com>
2021-10-30 15:12:02 +0600

    filter

--- lib/screens/exercises_page.dart
+++ lib/screens/exercises_page.dart
@@ -2,8 +2,13 @@ import 'package:fitness_app/common/const/color_const.dart';
 import 'package:fitness_app/domain/exercise.dart';
 import 'package:flutter/material.dart';
 
-class ExercisesPage extends StatelessWidget {
+class ExercisesPage extends StatefulWidget {
 
+  @override
+  State<ExercisesPage> createState() => _ExercisesPageState();
+}
+
+class _ExercisesPageState extends State<ExercisesPage> {
   final exercises = <Exercise>[
     Exercise(name: 'Жим лежа', muscleGroup: 'грудь', type: 'Базовый'),
     Exercise(name: 'Становая тяга', muscleGroup: 'спина', type: 'Базовый'),
@@ -14,9 +19,43 @@ class ExercisesPage extends StatelessWidget {
     Exercise(name: 'Кроссовер', muscleGroup: 'грудь', type: 'Изолированный'),
   ];
 
+  var filterTitle = '';
+  var filterTitleController = TextEditingController();
+  var filterMuscleGroup = 'Все группы';
+  var filterExerciseType = 'Все упражнении';
+
+  var filterText = '';
+  var filterHeight = 0.0;
+
+  List<Exercise> filter() {
+    setState(() {
+      filterText += '/' + filterMuscleGroup + '/' + filterExerciseType;
+      if (filterTitle.isNotEmpty) filterText += '/' + filterTitle;
+      filterHeight = 0;
+    });
+
+    var list = exercises;
+    return list;
+  }
+
+  List<Exercise> clearFilter() {
+    setState(() {
+      filterText = 'Все группы/Все упражнении';
+      filterTitle = '';
+      filterExerciseType = 'Все упражнении';
+      filterMuscleGroup = 'Все группы';
+      filterTitleController.clear();
+      filterHeight = 0;
+    });
+
+    var list = exercises;
+    return list;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
+    var exercisesList = Expanded(
+      child: ListView.builder(
       itemCount: exercises.length,
       itemBuilder: (context, index){
         return Card(
@@ -36,6 +75,124 @@ class ExercisesPage extends StatelessWidget {
           ),
         );
       },
+    ),
+    );
+    
+    var filterInfo = Container(
+      margin: EdgeInsets.only(top: 3, left: 7, right: 7, bottom: 5),
+      decoration: BoxDecoration(color: Colors.white.withOpacity(0.5)),
+      height: 40,
+      child: ElevatedButton(
+        child: Row(
+          children: <Widget>[
+            Icon(Icons.filter_list),
+            Text(
+              filterText,
+              style: TextStyle(),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+        onPressed: () {
+          setState(() {
+            filterHeight = (filterHeight == 0.0 ? 280.0 : 0.0);
+          });
+        },
+      ),
+    );
+
+    var exerciseTypeMenuItems = <String>[
+      'Все упражнении',
+      'Базовый',
+      'Изолированный'
+    ].map((String value1){
+      return DropdownMenuItem<String>(
+        value: value1,
+        child: Text(value1),
+      );
+    }).toList();
+
+    var muscleGroupMenuItems = <String>[
+      'Все группы',
+      'Спина',
+      'Грудь',
+      'Ноги',
+      'Плечи',
+      'Бицепс',
+      'Трицепс',
+    ].map((String value) {
+      return DropdownMenuItem<String>(
+        value: value,
+        child: Text(value),
+      );
+    }).toList();
+
+    var filterForm = AnimatedContainer(
+      margin: EdgeInsets.symmetric(vertical: 0.0, horizontal: 7),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(labelText: 'Группы мышц'),
+                items: muscleGroupMenuItems,
+                value: filterMuscleGroup,
+                onChanged: (String? val) => setState(() => filterMuscleGroup = val!),
+              ),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(labelText: 'Виды упражнении'),
+                items: exerciseTypeMenuItems,
+                value: filterExerciseType,
+                onChanged: (String? val) => setState(() => filterExerciseType = val!),
+              ),
+              TextFormField(
+                controller: filterTitleController,
+                decoration: const InputDecoration(labelText: 'Название'),
+                onChanged: (String val) => setState(() => filterTitle = val),
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        filter();
+                      },
+                      child:
+                          Text("Применить", style: TextStyle(color: Colors.white)),
+                      //color: AppColor.mainBackColor,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Expanded(
+                    flex: 1,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        clearFilter();
+                      },
+                      child:
+                          Text("Очистить", style: TextStyle(color: Colors.white)),
+                      //color: Colors.red,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      duration: const Duration(milliseconds: 400),
+      curve: Curves.fastOutSlowIn,
+      height: filterHeight,
+    );
+
+    return Column(
+      children: [
+        filterInfo,
+        filterForm,
+        exercisesList,
+      ],
     );
   }
 }
